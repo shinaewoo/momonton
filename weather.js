bbfaed61a8e99b1c541b84069ecab3faca343cac
shinaewoo <wooshinae92@gmail.com>
2020-06-14 23:54:50 +0900

    momonton

---
+++ weather.js
@@ -0,0 +1,52 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "f725af97d72242b07b4e97339270a293"
+const COORDS ='coords'
+
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    }
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function init() {
+    const loadedcoords = localStorage.getItem(COORDS);
+    if(loadedcoords === null) {
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedcoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+init();
